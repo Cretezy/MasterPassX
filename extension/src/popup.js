f96a324af29730445ae11e7ccffece88366cf43e
Charles Crete <cretezy@gmail.com>
2017-12-24 03:01:25 -0500

    Add chrome support to extension

--- extension/src/popup.js
+++ extension/src/popup.js
@@ -1,10 +1,21 @@
 (async () => {
 	let hostname = "";
 	try {
-		const tab = (await browser.tabs.query({active: true, currentWindow:true}))[0];
+		let tab;
+		if (window.browser) {
+			tab = (await window.browser.tabs.query({active: true, currentWindow: true}))[0];
+		} else if (window.chrome) {
+			tab = (await new Promise((resolve) => {
+				window.chrome.tabs.query({active: true, currentWindow: true}, resolve)
+			}))[0];
+		} else {
+			console.log("Browser unsupported.");
+		}
+		// console.log(tab, browser, chrome)
 		hostname = (new URL(tab.url)).hostname.replace("www.", "");
-	} catch (e) {
+	} catch (err) {
 		// Error parsing hostname
+		console.log(err);
 	}
 
 	const frame = document.createElement("iframe");
