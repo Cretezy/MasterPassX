2d6904596313f32412849ddde40982c7390877f0
Charles Crete <charles@cretezy.com>
2018-02-03 03:08:06 +0000

    Fix deleting user
    (ported from mobile branch)

--- web/src/store/users.js
+++ web/src/store/users.js
@@ -17,12 +17,12 @@ export function reducer(state = initialState, action) {
 				currentUserKey: action.key
 			};
 		case REMOVE_USER:
-			const users = state.users.filter(user => user.key === action.key);
+			const users = state.users.filter(user => user.key !== action.key);
 			let currentUserKey;
 			if (users.length === 0) {
 				currentUserKey = null; // No more users
 			} else if (!users.find(user => user.key === state.currentUserKey)) {
-				currentUserKey = users[0]; // Deleted current user
+				currentUserKey = users[0].key; // Deleted current user
 			} else {
 				currentUserKey = state.currentUserKey; // No change
 			}
